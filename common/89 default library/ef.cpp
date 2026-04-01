1dbcc588ed966b40b698637a37cb860e1e4a313c
kzc <kzc@users.noreply.github.com>
2026-04-01 09:30:48 -0400

    test <lang>_static_args clobbering normal args

--- common/89 default library/ef.cpp
+++ common/89 default library/ef.cpp
@@ -1,8 +1,16 @@
 #include"ef.h"
 
+#ifdef TEST_CPP_STATIC_ARGS
 DLL_PUBLIC Ef::Ef() : x(99) {
 }
+#else
+#error TEST_CPP_STATIC_ARGS not defined
+#endif
 
+#ifdef TEST_CPP_ARGS
 int DLL_PUBLIC Ef::get_x() const {
     return x;
 }
+#else
+#error TEST_CPP_ARGS not defined
+#endif
